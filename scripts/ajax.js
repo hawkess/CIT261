481fcdbff479801f4db9f431123c7184d3bca0ae
hawkess <50639790+hawkess@users.noreply.github.com>
2020-02-26 16:16:57 -0600

    Update ajax.js

--- scripts/ajax.js
+++ scripts/ajax.js
@@ -8,7 +8,7 @@ function parseWeather(response) {
   console.log(response);
   var weather = JSON.parse(response);
   var display = document.getElementById("ajax-content");
-  display.innerHTML += weather.name + ": " + weather.main.temp + "&#176;F, " + weather.description;
+  display.innerHTML += weather.name + ": " + weather.main.temp + "&#176;F, " + weather[0].description;
 }
 
 function requestWeather(city, key) {
