384751f10aadcfdcfdbdd0519f4bdd10995fe500
hawkess <50639790+hawkess@users.noreply.github.com>
2020-02-26 13:32:02 -0600

    update AJAX page

---
+++ scripts/ajax.js
@@ -0,0 +1,21 @@
+function ajaxTest() {
+  city = document.getElementById("city").value();
+  key = document.getElementById("api-key").value();
+  requestWeather(city, key);
+}
+
+function parseWeather(response) {
+  var weather = JSON.parse(response);
+  var display = document.getElementById("ajax-content");
+  display.innerHTML += weather.name + ": " + weather.main.temp + "&#176;F, " + weather.description;
+}
+
+function requestWeather(city, key) {
+  var xhttp = new XMLHttpRequest();
+  var uri = "api.openweathermap.org/data/2.5/weather?q=" + city + "&units=imperial&appid=" + key;
+  var res = encodeURI(uri);
+  xhttp.onreadystatechange = function() {
+    parseWeather(this.responseText);
+  };
+  xhttp.open("GET", uri, true);
+}
--- scripts/json.js
+++ scripts/json.js
@@ -2,17 +2,16 @@ window.onload = function() {
   var a = document.getElementById("json-copy");
   a.onclick = function() {
     if (window.clipboardData && window.clipboardData.setData) {
-      // Internet Explorer-specific code path to prevent textarea being shown while dialog is visible.
       return clipboardData.setData("Text", text);
 
     } else if (document.queryCommandSupported && document.queryCommandSupported("copy")) {
       var textarea = document.createElement("textarea");
       textarea.textContent = '{ "firstName": "John", "lastName": "Smith", "isAlive": true, "age": 27, "address": { "streetAddress": "21 2nd Street", "city": "New York", "state": "NY", "postalCode": "10021-3100" }, "phoneNumbers": [ { "type": "home", "number": "212 555-1234" }, { "type": "office", "number": "646 555-4567" }, { "type": "mobile", "number": "123 456-7890" } ], "children": [], "spouse": null }';
-      textarea.style.position = "fixed"; // Prevent scrolling to bottom of page in Microsoft Edge.
+      textarea.style.position = "fixed";
       document.body.appendChild(textarea);
       textarea.select();
       try {
-        return document.execCommand("copy"); // Security exception may be thrown by some browsers.
+        return document.execCommand("copy");
       } catch (ex) {
         console.warn("Copy to clipboard failed.", ex);
         return false;
