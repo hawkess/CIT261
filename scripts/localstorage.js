7ce2382a917dac73c17a6f5f3f80bf35bd880787
hawkess <50639790+hawkess@users.noreply.github.com>
2020-02-28 14:10:27 -0600

    localstorage javascript fixes

--- scripts/localstorage.js
+++ scripts/localstorage.js
@@ -1,30 +1,35 @@
-var itemsArray = localStorage.getItem("list-items") ? JSON.parse(localStorage.getItem("list-items")) : [];
-
 window.onload = function() {
+
+  let itemsArray = localStorage.getItem("list-items") ? JSON.parse(localStorage.getItem("list-items")) : [];
+
+  const form = document.getElementById("list-form");
+  const clearbtn = document.getElementById("clear-btn");
+  const ul = document.getElementById("demo-list");
+
+  form.addEventListener('submit', function(elem) {
+    elem.preventDefault();
+    let formInput = document.getElementById("list-item");
+    itemsArray.push(formInput.value);
+    localStorage.setItem("list-items", JSON.stringify(itemsArray));
+    makeList(formInput.value);
+    formInput.value = "";
+  });
+
+  clearbtn.addEventListener('click', function() {
+    localStorage.clear();
+    while (ul.firstChild) {
+      ul.removeChild(ul.firstChild);
+    }
+  });
+
+  function makeList(listItem) {
+    let li = document.createElement("li");
+    li.textContent = listItem;
+    ul.appendChild(li);
+  }
+
   itemsArray.forEach(item => {
     makeList(item);
   });
+
 };
-
-function clearStorage() {
-  const ul = document.getElementById("todo-list");
-  localStorage.clear();
-  while (ul.firstChild) {
-    ul.removeChild(ul.firstChild);
-  }
-}
-
-function storageTest() {
-  let formInput = document.getElementById("list-item");
-  itemsArray.push(formInput.value);
-  makeList(formInput.value);
-  formInput.value = "";
-  localStorage.setItem("items", JSON.stringify(itemsArray));
-}
-
-function makeList(listItem) {
-  const ul = document.getElementById("todo-list");
-  let li = document.createElement("li");
-  li.textContent = listItem;
-  ul.appendChild(li);
-}
