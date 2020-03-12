a28dc7901679d102c9ea8353fa77584151bf16fe
hawkess <50639790+hawkess@users.noreply.github.com>
2020-03-12 13:27:07 -0500

    Update javascript.js

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -15,7 +15,7 @@ function arrayTest() {
   let str = document.getElementById("array-input").value;
   let arr = str.split(", ");
   let len = arr.length;
-  document.getElementById("array-content").innerHTML += "Array: " + arr + "<br />";
+  document.getElementById("array-content").innerHTML += "Array: " + arr.join(', ') + "<br />";
   document.getElementById("array-content").innerHTML += "Array Length: " + len + "<br />";
   document.getElementById("array-content").innerHTML += "Array[0]: " + arr[0] + "<br />";
   document.getElementById("array-content").innerHTML += "Array[" + String(len - 1) + "]: " + arr[len - 1] + "<br />";
